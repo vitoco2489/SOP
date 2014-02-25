abe1a1500fd659c3c396bb5a57fb223387d723ad
vitoco89 <vitoco2489@gmail.com>
2014-02-25 18:19:27 -0300

    Pruebas Inicio Victor

--- WebUI/Controllers/UserController.cs
+++ WebUI/Controllers/UserController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WebUI.Models.test;
 
 namespace WebUI.Controllers
 {
@@ -15,6 +16,40 @@ namespace WebUI.Controllers
         {
             return View();
         }
+
+        public ActionResult Create()
+        {
+            if(Request.IsAjaxRequest())
+            {
+                return PartialView();
+            }
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(prueba prueba)
+        {
+            if (Request.IsAjaxRequest())
+            {
+                return PartialView();
+            }
+            return View();
+        }
+        public ActionResult Edit(int id = -1)
+        {
+            return View(new prueba{Apellido = "espinoza",Id=1,Nombre="victor"});
+        }
+        
+        [HttpPost]
+        public ActionResult Edit(prueba prueba)
+        {
+            return RedirectToAction("Detalles",prueba);
+        }
+
+        public ActionResult Detalles(prueba p)
+        {
+            return View(p);
+        }
+
 
         //agrega una vista parcia al view que lo ejecute
         [HttpPost]
---
+++ WebUI/Models/test/prueba.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebUI.Models.test
+{
+    public class prueba : BaseModels
+    {
+        public string Nombre { get; set; }
+        public string  Apellido { get; set; }
+    }
+
+
+
+
+}
