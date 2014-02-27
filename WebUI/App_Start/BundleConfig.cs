6a092124e17396782fa2ba38774e1b03fa3dd8f7
john <metalcob2@gmail.com>
2014-02-27 11:21:59 -0300

    john ejemplos ajax

--- WebUI/App_Start/BundleConfig.cs
+++ WebUI/App_Start/BundleConfig.cs
@@ -24,7 +24,7 @@ namespace WebUI
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
                         "~/Scripts/modernizr-*"));
 
-            bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/site.css"));
+            bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/site.css").Include("~/Content/stylemenu.css"));
 
             bundles.Add(new StyleBundle("~/Content/themes/base/css").Include(
                         "~/Content/themes/base/jquery.ui.core.css",
---
+++ WebUI/Controllers/TestAjaxController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebUI.Models.test;
+
+namespace WebUI.Controllers
+{
+    public class TestAjaxController : Controller
+    {
+        //
+        // GET: /TestAjax/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public JsonResult TestFromJqueryJson()
+        {
+            prueba a = new prueba() { Id = 1, Nombre = "John", Apellido = "Munoz" };
+            return Json(a);
+        }
+        [HttpPost]
+        public JsonResult TestListJsonFromJqueryJson()
+        {
+            List<prueba> jlist=new List<prueba>();
+            return Json(new List<prueba>(){new prueba(){Id = 1, Nombre = "John", Apellido = "Munoz"},new prueba(){Id = 2, Nombre = "Juan", Apellido = "Catalan"}});
+        }
+
+    }
+}
